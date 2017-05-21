f99d0b05359b540d0439fd6f21cad18a39147564
Steffen <steffen.butzer@outlook.com>
2017-05-21 20:34:41 +0200

    CI: build without TLS on travis and with valid certs on appveyor

--- appveyor.ps1
+++ appveyor.ps1
@@ -3,6 +3,7 @@
 
 $serverName = $env:COMPUTERNAME
 $instanceName = $args[0]
+$regInstance = $args[1]
 $smo = 'Microsoft.SqlServer.Management.Smo.'
 $wmi = new-object ($smo + 'Wmi.ManagedComputer')
 
@@ -21,6 +22,18 @@ $Np.Alter()
 # Set Alias
 New-Item HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client -Name ConnectTo | Out-Null
 Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo -Name '(local)' -Value "DBMSSOCN,$serverName\$instanceName" | Out-Null
+
+# Generate Certificate & use and trust it
+$cert = New-SelfSignedCertificate -DnsName $serverName,localhost -CertStoreLocation cert:\LocalMachine\My
+Get-Item -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$regInstance\MSSQLServer\SuperSocketNetLib" | New-ItemProperty -Name Certificate -Value $cert.Thumbprint.ToLower() -Force
+$rootStore = Get-Item cert:\LocalMachine\Root
+$rootStore.Open("ReadWrite")
+$rootStore.Add($cert)
+$rootStore.Close();
+
+# Bypass certificate (private key) permission errors by simply running as system
+# DONT DO THIS IN PRODUCTION OR REAL WORLD USE, assign proper permissions to the private key!
+sc.exe config "MSSQL`$$instanceName" obj= "LocalSystem" password= "dummy"
 
 # Start services
 Set-Service SQLBrowser -StartupType Manual
