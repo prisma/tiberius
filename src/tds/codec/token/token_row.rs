3d545dc02d669920e7c5ce091b7349d89f99a846
Julius de Bruijn <julius+github@nauk.io>
2020-06-03 16:08:30 +0200

    Rearrange token row

--- src/tds/codec/token/token_row.rs
+++ src/tds/codec/token/token_row.rs
@@ -6,6 +6,74 @@ pub struct TokenRow {
     data: Vec<ColumnData<'static>>,
 }
 
+impl IntoIterator for TokenRow {
+    type Item = ColumnData<'static>;
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
+}
+
+impl TokenRow {
+    /// Normal row. We'll read the metadata what we've cached and parse columns
+    /// based on that.
+    pub(crate) async fn decode<R>(src: &mut R) -> crate::Result<Self>
+    where
+        R: SqlReadBytes + Unpin,
+    {
+        let col_meta = src.context().last_meta().await.unwrap();
+
+        let mut row = Self {
+            data: Vec::with_capacity(col_meta.columns.len()),
+        };
+
+        for column in col_meta.columns.iter() {
+            let data = ColumnData::decode(src, &column.base.ty).await?;
+            row.data.push(data);
+        }
+
+        Ok(row)
+    }
+
+    /// SQL Server has packed nulls on this row type. We'll read what columns
+    /// are null from the bitmap.
+    pub(crate) async fn decode_nbc<R>(src: &mut R) -> crate::Result<Self>
+    where
+        R: SqlReadBytes + Unpin,
+    {
+        let col_meta = src.context().last_meta().await.unwrap();
+        let row_bitmap = RowBitmap::decode(src, col_meta.columns.len()).await?;
+
+        let mut row = Self {
+            data: Vec::with_capacity(col_meta.columns.len()),
+        };
+
+        for (i, column) in col_meta.columns.iter().enumerate() {
+            let data = if row_bitmap.is_null(i) {
+                column.base.null_value()
+            } else {
+                ColumnData::decode(src, &column.base.ty).await?
+            };
+
+            row.data.push(data);
+        }
+
+        Ok(row)
+    }
+
+    /// The number of columns.
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    /// Gives the columnar data with the given index. `None` if index out of
+    /// bounds.
+    pub fn get(&self, index: usize) -> Option<&ColumnData<'static>> {
+        self.data.get(index)
+    }
+}
+
 /// A bitmap of null values in the row. Sometimes SQL Server decides to pack the
 /// null values in the row, calling it the NBCROW. In this kind of tokens the row
 /// itself skips the null columns completely, but they can be found from the bitmap
@@ -47,73 +115,5 @@ impl RowBitmap {
         src.read_exact(&mut data[0..size]).await?;
 
         Ok(Self { data })
-    }
-}
-
-impl IntoIterator for TokenRow {
-    type Item = ColumnData<'static>;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.data.into_iter()
-    }
-}
-
-impl TokenRow {
-    /// Normal row. We'll read the metadata what we've cached and parse columns
-    /// based on that.
-    pub(crate) async fn decode<R>(src: &mut R) -> crate::Result<Self>
-    where
-        R: SqlReadBytes + Unpin,
-    {
-        let col_meta = src.context().last_meta().await.unwrap();
-
-        let mut row = TokenRow {
-            data: Vec::with_capacity(col_meta.columns.len()),
-        };
-
-        for column in col_meta.columns.iter() {
-            let data = ColumnData::decode(src, &column.base.ty).await?;
-            row.data.push(data);
-        }
-
-        Ok(row)
-    }
-
-    /// SQL Server has packed nulls on this row type. We'll read what columns
-    /// are null from the bitmap.
-    pub(crate) async fn decode_nbc<R>(src: &mut R) -> crate::Result<Self>
-    where
-        R: SqlReadBytes + Unpin,
-    {
-        let col_meta = src.context().last_meta().await.unwrap();
-        let row_bitmap = RowBitmap::decode(src, col_meta.columns.len()).await?;
-
-        let mut row = TokenRow {
-            data: Vec::with_capacity(col_meta.columns.len()),
-        };
-
-        for (i, column) in col_meta.columns.iter().enumerate() {
-            let data = if row_bitmap.is_null(i) {
-                column.base.null_value()
-            } else {
-                ColumnData::decode(src, &column.base.ty).await?
-            };
-
-            row.data.push(data);
-        }
-
-        Ok(row)
-    }
-
-    /// The number of columns.
-    pub fn len(&self) -> usize {
-        self.data.len()
-    }
-
-    /// Gives the columnar data with the given index. `None` if index out of
-    /// bounds.
-    pub fn get(&self, index: usize) -> Option<&ColumnData<'static>> {
-        self.data.get(index)
     }
 }
