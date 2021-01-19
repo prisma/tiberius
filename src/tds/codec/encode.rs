c83ffeec02202cfc702696e3402f81b026087bce
Julius de Bruijn <julius+github@nauk.io>
2021-01-19 16:49:17 +0100

    Use asynchronous-codec instead of futures-codec2

--- src/tds/codec/encode.rs
+++ src/tds/codec/encode.rs
@@ -1,12 +1,13 @@
 use super::{Packet, PacketCodec};
 use bytes::{BufMut, BytesMut};
-use futures_codec2::Encoder;
+use asynchronous_codec::Encoder;
 
 pub(crate) trait Encode<B: BufMut> {
     fn encode(self, dst: &mut B) -> crate::Result<()>;
 }
 
-impl Encoder<Packet> for PacketCodec {
+impl Encoder for PacketCodec {
+    type Item = Packet;
     type Error = crate::Error;
 
     fn encode(&mut self, item: Packet, dst: &mut BytesMut) -> Result<(), Self::Error> {
