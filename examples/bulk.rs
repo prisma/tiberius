7c45f821d88c16bde27e0accd9f8217a8eac90be
Roger (Peng) Yu <rogeryu@microsoft.com>
2022-06-09 16:42:19 -0700

    +1

--- examples/bulk.rs
+++ examples/bulk.rs
@@ -25,28 +25,50 @@ async fn main() -> anyhow::Result<()> {
 
     let mut client = Client::connect(config, tcp.compat_write()).await?;
 
+    // client
+    //     .execute("DROP TABLE IF EXISTS bulk_test1", &[])
+    //     .await?;
+    // info!("drop table");
+    // client
+    //     .execute("CREATE TABLE bulk_test1 (content int)", &[])
+    //     .await?;
+    // info!("create table done");
+    //
+    // let mut meta = BulkLoadMetadata::new();
+    // meta.add_column("content", TypeInfo::int(), ColumnFlag::Nullable);
+    //
+    // let mut req = client.bulk_insert("bulk_test1", meta).await?;
+    // let count = 1000i32;
+    //
+    // let pb = ProgressBar::new(count as u64);
+    //
+    // info!("start loading data");
+    // // for i in vec!["aaaaaaaaaaaaaaaaaaaa"; 1000].into_iter() {
+    // for i in 0..1000 {
+    //     let mut row = TokenRow::new();
+    //     row.push((i as i32).into_sql());
+    //     req.send(row).await?;
+    //     pb.inc(1);
+    // }
+
     client
         .execute("DROP TABLE IF EXISTS bulk_test1", &[])
         .await?;
-    info!("drop table");
     client
-        .execute("CREATE TABLE bulk_test1 (content int)", &[])
+        .execute("CREATE TABLE bulk_test1 (number INT)", &[])
         .await?;
-    info!("create table done");
 
     let mut meta = BulkLoadMetadata::new();
-    meta.add_column("content", TypeInfo::int(), ColumnFlag::Nullable);
+    meta.add_column("number", TypeInfo::int(), ColumnFlag::Nullable);
 
     let mut req = client.bulk_insert("bulk_test1", meta).await?;
-    let count = 1000i32;
+    let count = 2000i32;
 
     let pb = ProgressBar::new(count as u64);
 
-    info!("start loading data");
-    // for i in vec!["aaaaaaaaaaaaaaaaaaaa"; 1000].into_iter() {
-    for i in 0..1000 {
+    for (id, s) in vec!["aaaaaaaaaaaaaaaaaaaa"; 1000].into_iter().enumerate() {
         let mut row = TokenRow::new();
-        row.push((i as i32).into_sql());
+        row.push(((id * 2) as i32).into_sql());
         req.send(row).await?;
         pb.inc(1);
     }
