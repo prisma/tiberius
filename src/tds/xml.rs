b1970329a68727ce2650ec12b3a89631ce6df243
Flavian Desverne <flavian.dsv@gmail.com>
2024-06-06 16:32:59 +0200

    chore: clippy fixes + tests for zero-length encoding of varlen columns (#338)

--- src/tds/xml.rs
+++ src/tds/xml.rs
@@ -76,9 +76,9 @@ impl XmlData {
     }
 }
 
-impl ToString for XmlData {
-    fn to_string(&self) -> String {
-        self.data.to_string()
+impl std::fmt::Display for XmlData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.data)
     }
 }
 
--- tests/bulk.rs
+++ tests/bulk.rs
@@ -7,6 +7,8 @@ use std::sync::Once;
 use tiberius::{IntoSql, Result, TokenRow};
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
+use chrono::DateTime;
+#[cfg(all(feature = "tds73", feature = "chrono"))]
 use chrono::NaiveDateTime;
 
 use runtimes_macro::test_on_runtimes;
@@ -112,6 +114,22 @@ test_bulk_type!(smallint("SMALLINT", 2000, 0..2000i16));
 test_bulk_type!(int("INT", 2000, 0..2000i32));
 test_bulk_type!(bigint("BIGINT", 2000, 0..2000i64));
 
+test_bulk_type!(empty_varchar(
+    "VARCHAR(MAX)",
+    100,
+    vec![""; 100].into_iter()
+));
+test_bulk_type!(empty_nvarchar(
+    "NVARCHAR(MAX)",
+    100,
+    vec![""; 100].into_iter()
+));
+test_bulk_type!(empty_varbinary(
+    "VARBINARY(MAX)",
+    100,
+    vec![b""; 100].into_iter()
+));
+
 test_bulk_type!(real(
     "REAL",
     1000,
@@ -134,61 +152,69 @@ test_bulk_type!(varchar_limited(
 test_bulk_type!(datetime2(
     "DATETIME2",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
+
+#[cfg(all(feature = "tds73", feature = "chrono"))]
+test_bulk_type!(datetime2_naive("DATETIME2", 100, {
+    #[allow(deprecated)]
+    let dt = NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap();
+
+    vec![dt; 100].into_iter()
+}));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_0(
     "DATETIME2(0)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_1(
     "DATETIME2(1)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_2(
     "DATETIME2(2)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_3(
     "DATETIME2(3)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_4(
     "DATETIME2(4)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_5(
     "DATETIME2(5)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_6(
     "DATETIME2(6)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
 
 #[cfg(all(feature = "tds73", feature = "chrono"))]
 test_bulk_type!(datetime2_7(
     "DATETIME2(7)",
     100,
-    vec![NaiveDateTime::from_timestamp_opt(1658524194, 123456789).unwrap(); 100].into_iter()
+    vec![DateTime::from_timestamp(1658524194, 123456789); 100].into_iter()
 ));
