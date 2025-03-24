bf276876d3c04cef5ba00f3adfdf84a6d52375a9
Rasmus Kaj <rasmus.kaj@sr.se>
2025-03-24 11:21:50 +0100

    Implement multi_subnet_failover for tokio.

--- src/sql_browser/tokio.rs
+++ src/sql_browser/tokio.rs
@@ -2,8 +2,10 @@ use super::SqlBrowser;
 use crate::client::Config;
 use async_trait::async_trait;
 use futures_util::future::TryFutureExt;
+use futures_util::stream::FuturesUnordered;
+use futures_util::StreamExt;
 use net::{TcpStream, UdpSocket};
-use std::io;
+use std::{io, net::SocketAddr};
 use tokio::{
     net,
     time::{self, error::Elapsed, Duration},
@@ -18,57 +20,86 @@ impl SqlBrowser for TcpStream {
     async fn connect_named(builder: &Config) -> crate::Result<Self> {
         let addrs = net::lookup_host(builder.get_addr()).await?;
 
-        for mut addr in addrs {
-            if let Some(ref instance_name) = builder.instance_name {
-                // First resolve the instance to a port via the
-                // SSRP protocol/MS-SQLR protocol [1]
-                // [1] https://msdn.microsoft.com/en-us/library/cc219703.aspx
-
-                let local_bind: std::net::SocketAddr = if addr.is_ipv4() {
-                    "0.0.0.0:0".parse().unwrap()
-                } else {
-                    "[::]:0".parse().unwrap()
-                };
-
-                tracing::event!(
-                    Level::TRACE,
-                    "Connecting to instance `{}` using SQL Browser in port `{}`",
-                    instance_name,
-                    builder.get_port()
-                );
-
-                let msg = [&[4u8], instance_name.as_bytes()].concat();
-                let mut buf = vec![0u8; 4096];
-
-                let socket = UdpSocket::bind(&local_bind).await?;
-                socket.send_to(&msg, &addr).await?;
-
-                let timeout = Duration::from_millis(1000);
-
-                let len = time::timeout(timeout, socket.recv(&mut buf))
-                    .map_err(|_: Elapsed| {
-                        crate::error::Error::Conversion(
-                            format!(
-                                "SQL browser timeout during resolving instance {}. Please check if browser is running in port {} and does the instance exist.",
-                                instance_name,
-                                builder.get_port(),
-                            )
-                            .into(),
-                        )
-                    })
-                    .await??;
-
-                let port = super::get_port_from_sql_browser_reply(buf, len, instance_name)?;
-                tracing::event!(Level::TRACE, "Found port `{}` from SQL Browser", port);
-                addr.set_port(port);
-            };
-
-            if let Ok(stream) = TcpStream::connect(addr).await {
-                stream.set_nodelay(true)?;
-                return Ok(stream);
+        if builder.multi_subnet_failover {
+            let mut futures = addrs
+                .map(|addr| connect_addr(builder, addr))
+                .collect::<FuturesUnordered<_>>();
+            let mut first_error = None;
+            for f in futures.next().await {
+                match f {
+                    Ok(Some(result)) => return Ok(result),
+                    Ok(None) => break,
+                    Err(error) => {
+                        if first_error.is_none() {
+                            first_error = Some(error);
+                        }
+                    }
+                }
+            }
+            if let Some(error) = first_error {
+                return Err(error);
+            }
+        } else {
+            for addr in addrs {
+                if let Some(stream) = connect_addr(builder, addr).await? {
+                    return Ok(stream);
+                }
             }
         }
 
         Err(io::Error::new(io::ErrorKind::NotFound, "Could not resolve server host").into())
+    }
+}
+
+async fn connect_addr(builder: &Config, mut addr: SocketAddr) -> crate::Result<Option<TcpStream>> {
+    if let Some(ref instance_name) = builder.instance_name {
+        // First resolve the instance to a port via the
+        // SSRP protocol/MS-SQLR protocol [1]
+        // [1] https://msdn.microsoft.com/en-us/library/cc219703.aspx
+
+        let local_bind: std::net::SocketAddr = if addr.is_ipv4() {
+            "0.0.0.0:0".parse().unwrap()
+        } else {
+            "[::]:0".parse().unwrap()
+        };
+
+        tracing::event!(
+            Level::TRACE,
+            "Connecting to instance `{}` using SQL Browser in port `{}`",
+            instance_name,
+            builder.get_port()
+        );
+
+        let msg = [&[4u8], instance_name.as_bytes()].concat();
+        let mut buf = vec![0u8; 4096];
+
+        let socket = UdpSocket::bind(&local_bind).await?;
+        socket.send_to(&msg, &addr).await?;
+
+        let timeout = Duration::from_millis(1000);
+
+        let len = time::timeout(timeout, socket.recv(&mut buf))
+            .map_err(|_: Elapsed| {
+                crate::error::Error::Conversion(
+                    format!(
+                        "SQL browser timeout during resolving instance {}. Please check if browser is running in port {} and does the instance exist.",
+                        instance_name,
+                        builder.get_port(),
+                    )
+                    .into(),
+                )
+            })
+            .await??;
+
+        let port = super::get_port_from_sql_browser_reply(buf, len, instance_name)?;
+        tracing::event!(Level::TRACE, "Found port `{}` from SQL Browser", port);
+        addr.set_port(port);
+    };
+
+    if let Ok(stream) = TcpStream::connect(addr).await {
+        stream.set_nodelay(true)?;
+        return Ok(Some(stream));
+    } else {
+        return Ok(None);
     }
 }
